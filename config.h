4c1ccf3486e43064750a9fddd73ac43221b31f11
Tyler McClanahan <tmak1787@gmail.com>
2021-04-06 12:31:39 -0600

    font size increase

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:size=13:antialias=true:autohint=true";
+static char *font = "Fira Code:size=14:antialias=true:autohint=true";
 static int borderpx = 5;
 
 /*

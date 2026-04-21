d9d8531f1bfa8f1912b7eea43e1c751999f913f5
Tyler McClanahan <tmak1787@gmail.com>
2026-04-21 12:18:52 -0600

    change font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IBM Plex Mono:size=14:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:size=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
